b1be607402fd6bfefcc730ad88bfb8f71bc43d39
monicahmoh4-svg <monicahmoh4@gmail.com>
2026-06-07 17:30:24 +0300

    Update notifications.js

--- backend/src/routes/notifications.js
+++ backend/src/routes/notifications.js
@@ -1,5 +1,2 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
 const { notifRouter } = require('./analytics');
 module.exports = notifRouter;
